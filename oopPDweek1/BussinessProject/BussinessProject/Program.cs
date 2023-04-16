0ad763a1fa4af099941b4cde965699d3fd5f8787
altaf <muskanaltaf116@gmail.com>
2023-04-16 21:42:37 +0500

    ooptask

---
+++ oopPDweek1/BussinessProject/BussinessProject/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 100;
+            int productQuantity = 20;
+            string[] name = new string[size];
+            string[] password = new string[size];
+            string[] foodItem = new string[productQuantity];
+            int[] price = new int[productQuantity];
+            string role;
+            string[] roleName = new string[5];
+            
+            int option;
+            do
+            {
+
+                readDATA(size, name, password);
+                Console.Clear();
+                option = menu();
+                Console.Clear();
+                if (option == 1)
+                {
+                    Console.WriteLine("enter name :");
+                    string n = Console.ReadLine();
+                    Console.WriteLine("enter password :");
+                    string p = Console.ReadLine();
+                    Console.WriteLine("enter your role :");
+                    role = Console.ReadLine();
+                    signUP(n, p, role);
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("enter name :");
+                    string n = Console.ReadLine();
+                    Console.WriteLine("enter password :");
+                    string p = Console.ReadLine();
+                    string result;
+                    result = signIN(size, n, p, name, password, roleName);
+                    if (result == "admin")
+                    {
+                        int output = (adminMenu(foodItem, price, productQuantity));
+                    }
+                }
+            }
+            while (option < 3);
+                    {
+                        Console.WriteLine("invalid option!!!");
+                    }
+            
+        }
+        static int menu()
+        {
+            int option;
+            Console.WriteLine("1.Sign up");
+            Console.WriteLine("2.Sign in");
+            Console.WriteLine("3.exit");
+            Console.WriteLine("Enter option :");
+            option = int.Parse(Console.ReadLine());
+            return option;
+           
+                
+
+        }
+        static string getParse(string record, int field)
+        {
+            int count = 1;
+            string item = "";
+            for (int x = 0; x < record.Length; x++)
+            {
+                if (record[x] == ',')
+                {
+                    count++;
+                }
+                else if (count == field)
+                {
+                    item = item + record[x];
+                }
+
+            }
+            return item;
+        }
+        static void readDATA(int  size, string[] name, string[] password)
+        {
+            int x = 0;
+            string path = " G:\\oopweek1\\week1\\week1\\data.txt";
+            if (File.Exists(path))
+            {
+                StreamReader fileVariable = new StreamReader(path);
+                string record;
+                while ((record = fileVariable.ReadLine()) != null)
+                {
+                    name[x] = getParse(record, 1);
+                    password[x] = getParse(record, 2);
+                    x++;
+                    if (size >= 100)
+                    {
+                        break;
+                    }
+
+
+                }
+                fileVariable.Close();
+            }
+            else
+            {
+                Console.WriteLine("No Exists!!!");
+            }
+
+        }
+         static string signIN(int size,string n,string p,string[] name,string[] password,string[] roleName)
+        {
+          
+            string roles= "null";
+            for (int x = 0; x <size;x++)
+            {
+
+                if (n == name[x] && p == password[x] )
+                {
+                   
+                    roles = roleName[x];
+                    break;
+                 
+                }
+            }
+            return roles;
+            
+
+        }
+        static void signUP( string n, string p,string role)
+        {
+            string path = " G:\\oopweek1\\week1\\week1\\data.txt";
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(n + "," + p + "," + role);
+            file.Flush();
+            file.Close();
+
+        }
+        static void writefoodItem(string[] foodItem, int[] price)
+        {
+            string path = "G:\\oopPDweek1\\HotelMenu.txt";
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(foodItem+ "," +price);
+            file.Flush();
+            file.Close();
+
+        }
+        static void listofFoodItems(string[]foodItem,int[] price,int productQuantity)
+        {
+            int idx = 0;
+            string path = "G:\\oopPDweek1\\HotelMenu.txt";
+            if (File.Exists(path))
+            {
+                StreamReader file = new StreamReader(path);
+                string menu;
+                while ((menu = file.ReadLine()) != null)
+                {
+                    foodItem[idx] = getParse(menu, 2);
+                    price[idx] = int.Parse(getParse(menu, 3));
+    
+                    if (productQuantity >= 20)
+                    {
+                        break;
+                    }
+
+                }
+                file.Close();
+            }
+                
+                else
+                {
+                    Console.WriteLine("No eXists!!!");
+                }
+            Console.ReadKey();
+            
+        }
+        static void deleteFood(string[] foodItem, int[] price)
+        {
+            string path = "G:\\oopPDweek1\\HotelMenu.txt";
+            int idx = 0;
+            string record;
+            if (File.Exists(path))
+            {
+                StreamReader file = new StreamReader(path);
+                while ((record = file.ReadLine()) != null)
+                {
+
+
+                    string foodName = Console.ReadLine();
+                    Console.WriteLine("enter food name you want to delete :");
+                    foodItem[idx] = " remove";
+                    idx++;
+
+
+
+
+                }
+            }
+        }
+        static void updatefood (string[] foodItem, int[] price)
+        {
+            string path = "G:\\oopPDweek1\\HotelMenu.txt";
+            int idx = 0;
+            string record;
+            if(File.Exists(path))
+            {
+                StreamReader file = new StreamReader(path);
+                while((record =file.ReadLine())!= null)
+                    {
+
+
+                    string foodName= Console.ReadLine();
+                    Console.WriteLine("enter food name :");
+                    foodItem[idx] = foodName;
+                    idx++;
+
+
+
+
+                }
+            }
+
+            
+
+            
+            
+        }
+        static int   adminMenu(string[] foodItem, int[] price, int productQuantity)
+        {
+            int choice;
+            Console.WriteLine("1.view the list of food item");
+            Console.WriteLine("2.update food ");
+            Console.WriteLine("3.delete food ");
+            Console.WriteLine("enter your option : ");
+            choice = int.Parse(Console.ReadLine());
+            return choice;
+            if(choice ==1)
+            {
+                writefoodItem(foodItem, price);
+            }
+            else if(choice ==2)
+            {
+                updatefood(foodItem, price);
+            }
+            else if (choice ==3)
+            {
+                deleteFood(foodItem, price);
+            }
+        }
+    }
+}
---
+++ oopPDweek1/BussinessProject/BussinessProject/game.cs
@@ -0,0 +1,217 @@
+﻿using EZInput;
+using System;
+using System.Threading;
+
+namespace Project1
+{
+    class Program
+    {
+        static int score = 0;
+        static void Main(string[] args)
+        {
+            int RabbitX = 1;
+            int RabbitY = 1;
+            char[,] rabbit = new char[5, 8];
+
+            int trollX = 18;
+            int trollY = 9;
+
+            char[,] troll = new char[3, 9];
+            string troll = "down";
+
+            int gobblinX = 20;
+            int gobblinY 10;
+
+            char[,] gobblin = new char[7, 15];
+            string gobblin = "down";
+
+            int[]  bullletX = new int [1000];
+            int[]  bulletY = new int [1000];
+            int bcount = 0;
+
+            char[,] maze = new char[50, 50];
+
+
+
+
+            
+            {
+            { '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%'},
+            { '%', ' ', ' ', '.', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', '.', ' ', '.', ' ', ' ', ' ', ' ', ' ', ' ', '.', ' ', '.', ' ', '.', ' ', '%'},
+            { '%', ' ', '.', ' ', ' ', '.', '.', '.', ' ', ' ', ' ', ' ', '.', '.', '.', ' ', ' ', '.', ' ', '%'},
+            { '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%', '%'}};
+            printMaze(maze);
+            Console.SetCursorPosition(trollY, trollX);
+            Console.SetCursorPosition(RabbitY, RabbitX);
+            Console.Write("S");
+
+            while (true)
+            {
+                Thread.Sleep(150);
+                printScore();
+                if (Keyboard.IsKeyPressed(Key.UpArrow))
+                {
+                    moveRabbitUp(maze, ref RabbitX, ref RabbitY);
+                }
+                if (Keyboard.IsKeyPressed(Key.DownArrow))
+                {
+                    moveSnakeDown(maze, ref RabbitX, ref RabbitY);
+                }
+                if (Keyboard.IsKeyPressed(Key.LeftArrow))
+                {
+                    moveSnakeLeft(maze, ref RabbitX, ref RabbitY);
+                }
+                if (Keyboard.IsKeyPressed(Key.RightArrow))
+                {
+                    moveRabbitRight(maze, ref snakeX, ref snakeY);
+                }
+                moveEnemy(maze, ref enemyX, ref enemyY, ref enemydirection);
+
+            }
+        }
+        static void printMaze(char[,]maze)
+        {
+            for (int row = 0; row < 20; row++)
+            {
+                for (int col = 0; col < 20; col++)
+                {
+                    Console.Write(maze[row,col]);
+                }
+            Console.WriteLine();
+            }
+        }
+        static void moveRabbitUp(char[,] maze, ref int RabbitX, ref int RabbitY)
+        {
+            if (maze[RabbitX - 1, RabbitY] == ' ' || maze[RabbitX - 1, RabbitY] == '.')
+            {
+                maze[RabbitX, RabbitY] = ' ';
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                Console.Write(" ");
+                RabbitX = RabbitX - 1;
+                if (maze[RabbitX, RabbitY] == '.')
+                {
+                    calculateScore();
+                }
+                maze[snakeX, snakeY] = 'R';
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                Console.Write("R");
+            }
+        }
+        static void moveSnakeDown(char[,] maze, ref int RabbitX, ref int RabbitY)
+        {
+            if (maze[RabbitX + 1, RabbitY] == ' ' || maze[RabbitX + 1, RabbitY] == '.')
+            {
+                maze[RabbitX, RabbitY] = ' ';
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                Console.Write(" ");
+                RabbitX = RabbitX + 1;
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                if (maze[RabbitX, RabbitY] == '.')
+                {
+                    calculateScore();
+                }
+                maze[RabbitX, RabbitY] = 'R';
+                Console.Write("R");
+            }
+        }
+        static void moveSnakeLeft(char[,] maze, ref int snakeX, ref int snakeY)
+        {
+            if (maze[RabbitX, RabbitY - 1] == ' ' || maze[RabbitX, RabbitY - 1] == '.')
+            {
+                maze[RabbitX, RabbitY] = ' ';
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                Console.Write(" ");
+                RabbitY = RabbitY - 1;
+                if (maze[RabbitX, RabbitY] == '.')
+                {
+                    calculateScore();
+                }
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                maze[RabbitX, RabbitY] = 'R';
+                Console.Write("R");
+            }
+        }
+        static void moveSnakeRight(char[,] maze, ref int RabbitX, ref int RabbitY)
+        {
+            if (maze[RabbitX, RabbitY + 1] == ' ' || maze[RabbitX, RabbitY + 1] == '.')
+            {
+                maze[RabbitX, RabbitY] = ' ';
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                Console.Write(" ");
+                RabbitY = RabbitY + 1;
+                if (maze[RabbitX, RabbitY] == '.')
+                {
+                    calculateScore();
+                }
+                Console.SetCursorPosition(RabbitY, RabbitX);
+                maze[RabbitX, RabbitY] = 'R';
+                Console.Write("R");
+            }
+        }
+        static void movetroll(char[,] maze, ref int trollX, ref int trollY, ref string trolldirection)
+        {
+            if (maze[trollX, trollY - 1] == 'R' || maze[trollX, trollY + 1] == 'R' || maze[trollX + 1, trollY] == 'R' || maze[trollX - 1, trollY] == 'R')
+            {
+                Environment.Exit(0);
+            }
+            if (enemydirection == "Up")
+            {
+                if (maze[trollX - 1, trollY] == ' ')
+                {
+                    maze[trollX, trollY] = ' ';
+                    Console.SetCursorPosition(trollY, trollX);
+                    Console.Write(" ");
+                    trollX = trollX - 1;
+                    Console.SetCursorPosition(trollY, trollX);
+                    Console.Write("T");
+                }
+                else
+                {
+                    trolldirection = "Down";
+                }
+            }
+            if (trolldirection == "Down")
+            {
+                if (maze[trollX + 1, trollY] == ' ')
+                {
+                    maze[trollX, trollY] = ' ';
+                    Console.SetCursorPosition(trollY, trollX);
+                    Console.Write(" ");
+                    trollX = trollX + 1;
+                    Console.SetCursorPosition(trollY, trollX);
+                    Console.Write("T");
+                }
+                else
+                {
+                    enemydirection = "Up";
+                }
+            }
+        }
+        static void printScore()
+        {
+            Console.SetCursorPosition(3, 21);
+            Console.WriteLine("SCORE: " + score);
+        }
+        static void calculateScore()
+        {
+            score ++;
+        }
+    }
+}
+
